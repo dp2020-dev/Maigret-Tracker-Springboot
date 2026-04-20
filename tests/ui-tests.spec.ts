4831f6448e58e3d9407b16a83d9e589b0d133771
Daniel Phillips <danielphillips@Daniels-MacBook-Pro.local>
2026-04-20 18:09:55 +0100

    Bug fix- now able to save notes

--- tests/ui-tests.spec.ts
+++ tests/ui-tests.spec.ts
@@ -2,35 +2,28 @@ import { test, expect } from './fixtures/base.fixture';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Maigret Tracker/);
-});
+  });
 
 test('search word not found, returns no results', async ({ indexPage }) => {
   await indexPage.goto();
   await indexPage.performSearch('green');
   await expect(indexPage.noResultsMessage).toBeVisible();
   await expect(indexPage.noResultsMessage).toHaveText('No books found.');
-});
+  });
 
 test('search word found, returns expected book', async ({ indexPage }) => {
   await indexPage.goto();
   await indexPage.performSearch('yellow');
   await indexPage.getBookCardByTitle("The Yellow Cat")
-});
-
+  });
 
 test('verify books read + unread books = total books', async ({ indexPage }) => {
   await indexPage.goto();
-
-
   await indexPage.countBooks();
-  
-});
+  });
 
-test('verify progress percentage.', async ({ page, indexPage }) => {
+test('verify progress percentage.', async ({ indexPage }) => {
   await indexPage.goto();
   await indexPage.percentComplete();
-  
-});
+  });
